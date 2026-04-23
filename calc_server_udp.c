ab3c89a35a2ce7336d89a9b7e31fcee466d85730
Mukul Kumar <mukul.kumar.it28@heritageit.edu.in>
2026-04-23 19:32:53 +0530

    Create calc_server_udp.c

---
+++ calc_server_udp.c
@@ -0,0 +1,80 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+struct sockaddr_in serv_addr, cli_addr;
+
+int sockfd, r, w, cli_addr_len;
+unsigned short serv_port = 25020;
+char serv_ip[] = "127.0.0.1";
+char buff[128], reply[128];
+
+int main()
+{
+    bzero(&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(serv_port);
+    inet_aton(serv_ip, (&serv_addr.sin_addr));
+
+    printf("\nUDP CALCULATOR SERVER.\n");
+
+    if((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    {
+        printf("\nSERVER ERROR: Cannot create socket.\n");
+        exit(1);
+    }
+
+    if((bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr))) < 0)
+    {
+        printf("\nSERVER ERROR: Cannot bind.\n");
+        close(sockfd);
+        exit(1);
+    }
+
+    cli_addr_len = sizeof(cli_addr);
+    printf("\nSERVER: Waiting for calculation requests...\n");
+
+    while(1)
+    {
+        bzero(buff, 128);
+        if((r = recvfrom(sockfd, buff, 128, 0, (struct sockaddr*)&cli_addr, &cli_addr_len)) < 0)
+        {
+            printf("\nSERVER ERROR: Cannot receive.\n");
+            continue;
+        }
+        buff[r] = '\0';
+
+        if(strncmp(buff, "exit", 4) == 0)
+        {
+            printf("\nSERVER: Client disconnected.\n");
+            continue;
+        }
+
+        char op;
+        int a, b;
+        sscanf(buff, "%d %c %d", &a, &op, &b);
+
+        switch(op)
+        {
+            case '+': sprintf(reply, "%d + %d = %d\n", a, b, a + b); break;
+            case '-': sprintf(reply, "%d - %d = %d\n", a, b, a - b); break;
+            case '*': sprintf(reply, "%d * %d = %d\n", a, b, a * b); break;
+            case '/': 
+                if(b != 0)
+                    sprintf(reply, "%d / %d = %.2f\n", a, b, (float)a / b);
+                else
+                    sprintf(reply, "Error: Division by zero\n");
+                break;
+            default: sprintf(reply, "Invalid format. Use: number operator number\n");
+        }
+
+        if((w = sendto(sockfd, reply, strlen(reply), 0, (struct sockaddr*)&cli_addr, cli_addr_len)) < 0)
+            printf("\nSERVER ERROR: Cannot send.\n");
+    }
+    close(sockfd);
+    return 0;
+}
